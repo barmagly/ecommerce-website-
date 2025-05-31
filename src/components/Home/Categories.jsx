6f79e7155e9e8d9b4b0a7eeff8d7f31834c51fc8
MagySamwel <maggi1samwel@gmail.com>
2025-05-31 03:40:38 +0300

    home sections

---
+++ src/components/Home/Categories.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { Box, Grid, IconButton, useMediaQuery, useTheme } from '@mui/material';
+import { useState, useEffect } from 'react';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const categories = [
+    {
+        name: 'الكمبيوتر',
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'الكمبيوتر',
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'الكمبيوتر',
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'الكمبيوتر',
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'الكمبيوتر',
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'الكمبيوتر',
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+]
+
+const Categories = () => {
+    const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const isTablet = useMediaQuery(theme.breakpoints.between('sm', 'lg'));
+  const isDesktop = useMediaQuery(theme.breakpoints.up('lg'));
+
+  const [page, setPage] = useState(0);
+  const [productsPerPage, setProductsPerPage] = useState(6);
+
+  useEffect(() => {
+    if (isMobile) {
+      setProductsPerPage(1);
+    } else if (isTablet) {
+      setProductsPerPage(3);
+    } else if (isDesktop) {
+      setProductsPerPage(6);
+    }
+  }, [isMobile, isTablet, isDesktop]);
+
+  const pageCount = Math.ceil(categories.length / productsPerPage);
+  const startIdx = page * productsPerPage;
+  const endIdx = startIdx + productsPerPage;
+  const currentProducts = categories.slice(startIdx, endIdx);
+  
+  const arrowSection=()=>{
+    const handlePrev = () => {
+      setPage((prev) => (prev > 0 ? prev - 1 : prev));
+    };
+    const handleNext = () => {
+      
+      setPage((prev) => (prev < pageCount - 1 ? prev + 1 : prev));
+      console.log(currentProducts);
+    };
+    return(
+      <Box>
+      <IconButton sx={{ background: 'rgb(245, 245, 245)', borderRadius: 12 }} onClick={handlePrev} disabled={page <= 0}>
+        <ArrowForwardIcon />
+      </IconButton>
+      <IconButton sx={{ background: 'rgb(245, 245, 245)', borderRadius: 12 }} onClick={handleNext} disabled={page >= pageCount - 1}>
+        <ArrowBackIcon />
+      </IconButton>
+    </Box>
+    )
+  }
+  return (
+    <Grid p={{ xs: 5 ,md:8}} >
+      <Box display={'flex'} flexDirection={{ xs: 'column', md: 'row' }} justifyContent={{ xs: 'center', md: 'space-between' }} alignItems={'center'}>
+      <div style={{ color: '#E94560', fontWeight: 'bold', fontSize: 24 }}>
+        التصنيفات
+      </div>
+        {arrowSection()}
+      </Box>
+      <Grid style={{ display: 'flex', gap: 24, flexWrap: 'wrap' }} py={3} justifyContent={{ xs: 'center', md: 'space-evenly' }} alignItems={'center'}>
+        {currentProducts.map((product, idx) => (
+            <div key={idx} style={{ background: '#fafafa', borderRadius: 12,border: '1px solid black', padding: 10, width: 180, position: 'relative', boxShadow: '0 2px 8px #eee' }}>
+                <img src={product.image} alt={product.name} style={{ width: '100%', height: 180, objectFit: 'contain', marginBottom: 12 }} />
+                <div style={{ fontWeight: 'bold', fontSize: 16, marginBottom: 8, textAlign: 'center' }}>{product.name}</div>
+            </div>
+        ))}
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Categories
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,118 @@
 import React from 'react'
+import FlashSales from '../components/Home/FlashSales'
+import Categories from '../components/Home/Categories'
+import Products from '../components/Home/Products'
+const products = [
+    {
+        name: 'لوحة ألعاب HAVIT HV-G92',
+        price: '١٢٠ ج.م',
+        oldPrice: '١٦٠ ج.م',
+        discount: '٤٠٪ خصم',
+        rating: 5,
+        reviews: 88,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'لوحة مفاتيح AK-900 سلكية',
+        price: '٩٦٠ ج.م',
+        oldPrice: '١١٦٠ ج.م',
+        discount: '٣٥٪ خصم',
+        rating: 4,
+        reviews: 75,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'شاشة ألعاب IPS LCD',
+        price: '٣٧٠ ج.م',
+        oldPrice: '٤٠٠ ج.م',
+        discount: '٣٠٪ خصم',
+        rating: 5,
+        reviews: 99,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'كرسي مريح من سلسلة S',
+        price: '٣٧٥ ج.م',
+        oldPrice: '٤٠٠ ج.م',
+        discount: '٢٥٪ خصم',
+        rating: 4.5,
+        reviews: 99,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/0tl68r54_expires_30_days.png',
+    },
+    {
+        name: 'سماعات رأس لاسلكية',
+        price: '٢٥٠ ج.م',
+        oldPrice: '٣٠٠ ج.م',
+        discount: '١٧٪ خصم',
+        rating: 4,
+        reviews: 60,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/w21oxegn_expires_30_days.png  ',
+    },
+    {
+        name: 'ماوس ألعاب احترافي',
+        price: '١٨٠ ج.م',
+        oldPrice: '٢٢٠ ج.م',
+        discount: '١٨٪ خصم',
+        rating: 4.5,
+        reviews: 45,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/w21oxegn_expires_30_days.png  ',
+    },
+    {
+        name: 'ساعة ذكية',
+        price: '٥٥٠ ج.م',
+        oldPrice: '٦٠٠ ج.م',
+        discount: '٨٪ خصم',
+        rating: 4,
+        reviews: 80,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/w21oxegn_expires_30_days.png  ',
+    },
+    {
+        name: 'هاتف ذكي',
+        price: '٣٠٠٠ ج.م',
+        oldPrice: '٣٥٠٠ ج.م',
+        discount: '١٤٪ خصم',
+        rating: 5,
+        reviews: 120,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/w21oxegn_expires_30_days.png  ',
+    },
+    {
+        name: 'جهاز لوحي',
+        price: '١٥٠٠ ج.م',
+        oldPrice: '١٧٠٠ ج.م',
+        discount: '١٢٪ خصم',
+        rating: 4,
+        reviews: 70,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/h9xc0rsa_expires_30_days.png  ',
+    },
+    {
+        name: 'كاميرا رقمية',
+        price: '٢٢٠٠ ج.م',
+        oldPrice: '٢٥٠٠ ج.م',
+        discount: '١٢٪ خصم',
+        rating: 4.5,
+        reviews: 55,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/h9xc0rsa_expires_30_days.png  ',
+    },
+    {
+        name: 'مكبر صوت بلوتوث',
+        price: '٣٠٠ ج.م',
+        oldPrice: '٣٥٠ ج.م',
+        discount: '١٤٪ خصم',
+        rating: 4,
+        reviews: 40,
+        image: 'https://storage.googleapis.com/tagjs-prod.appspot.com/v1/SWeYrJ75rl/h9xc0rsa_expires_30_days.png  ',
+    }
 
-
+];
 function Home() {
+    
+    
     return (
         <>
             <div>home</div>
+            <FlashSales products={products} />
+            <Categories />
+            <Products products={products} />
         </>
     )
 }
