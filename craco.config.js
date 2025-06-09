bc13a0551ebf8fb36b7cfd3e565fc21d630ee07d
Ahmed Kamal <57872185+AhmedKamalMo@users.noreply.github.com>
2025-06-09 12:25:34 +0300

    handel depoly error

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,29 @@
 module.exports = {
-  webpack: {
-    configure: (webpackConfig) => {
-      webpackConfig.ignoreWarnings = [
-        (warning) =>
-          warning.message &&
-          warning.message.includes('Failed to parse source map from') &&
-          warning.module &&
-          warning.module.resource &&
-          warning.module.resource.includes('stylis-plugin-rtl')
-      ];
-      return webpackConfig;
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
   },
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          "path": require.resolve("path-browserify"),
+          "os": require.resolve("os-browserify/browser"),
+          "crypto": require.resolve("crypto-browserify"),
+          "stream": require.resolve("stream-browserify"),
+          "http": require.resolve("stream-http"),
+          "https": require.resolve("https-browserify"),
+          "zlib": require.resolve("browserify-zlib"),
+          "assert": require.resolve("assert/"),
+          "url": require.resolve("url/"),
+          "buffer": require.resolve("buffer/"),
+          "fs": false
+        }
+      }
+    }
+  }
 }; 
